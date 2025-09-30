d1cafc21cfb7716d2c63e1bf9a95839b45aa1a33
Daniel Arcade <83617030+DanielArcade@users.noreply.github.com>
2025-09-30 19:12:35 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo cp "/userdata/system/batocera_key.key" "/userdata/system"
+sudo cp "/userdata/system/key/batocera_key.key" "/userdata/system/"
 sudo chmod 644 "/userdata/system/batocera_key.key"
 sudo chown arcade:arcade "/userdata/system/batocera_key.key"
+
 
 sleep 5
 
