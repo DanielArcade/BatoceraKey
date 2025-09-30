e7d9d58cfcf4a548c8da6687c53e4c67c068951d
Daniel Arcade <83617030+DanielArcade@users.noreply.github.com>
2025-09-30 20:28:14 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Dá permissão total para todos os usuários em /userdata/system e tudo dentro dela
+# Garante permissão total em /userdata/system
 chmod -R 777 "/userdata/system"
 
-sleep 5
-
-# Copia o arquivo batocera_key.key para /userdata/system
+# Copia o arquivo
 cp "/userdata/system/key/batocera_key.key" "/userdata/system/"
 
-sleep 10
-
-# Remove a pasta clonada se existir
+# Remove a pasta clonada
 [ -d /userdata/system/key ] && rm -rf /userdata/system/key
 
 exit 0
