3001b664283b6cedf202a4bd81e43fdac18d52f5
Daniel Arcade <83617030+DanielArcade@users.noreply.github.com>
2025-09-30 19:19:39 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ sudo cp "/userdata/system/key/batocera_key.key" "/userdata/system"
 sleep 1
 
 # Remove a pasta clonada com segurança (só se existir)
-[ -d /userdata/system/key ] && sudo rm -rf /userdata/system/key
+#[ -d /userdata/system/key ] && sudo rm -rf /userdata/system/key
 
 # Limpa a lixeira (pasta do Trash — opcional, mas cuidado ao usar)
 #[ -d /home/arcade/.local/share/Trash ] && sudo rm -rf /home/arcade/.local/share/Trash
